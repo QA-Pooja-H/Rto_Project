bc91e826f3f58c4d3b5b885b7dc1b5c2fc8bddf1
Sunil <Sunil@DESKTOP-5KRD00M>
2025-04-23 16:28:45 +0530

    change msg in utility class

--- Utility project/src/utility.java
+++ Utility project/src/utility.java
@@ -1,12 +1,14 @@
 
-public class utility { 
-	void LoginCheck(String un,String pw) {
-	System.out.println("Home page")	;
-	
-		if (un =="xyz" && pw=="231dcsd") {
-	}else {
-		System.out.println("Invalid Credentials");
-	}
+public class utility {
+	void LoginCheck(String un, String pw) {
+		System.out.println("Home page");
+
+		if (un == "xyz" && pw == "231dcsd") {
+
+		} else {
+
+			System.out.println("Incorrect Username Or Passworld");
+		}
 	}
 
 }
