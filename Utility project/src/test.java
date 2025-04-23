672308d6317e71ac4ee0687b8a4bee1d07c1d729
Sunil <Sunil@DESKTOP-5KRD00M>
2025-04-23 16:13:58 +0530

    first commit

---
+++ Utility project/src/test.java
@@ -0,0 +1,8 @@
+
+public class test {
+	public static void main(String[]args) {
+		utility var= new utility();
+		var.LoginCheck("xyz", "231dcsd");
+	}
+
+}
---
+++ Utility project/src/utility.java
@@ -0,0 +1,12 @@
+
+public class utility { 
+	void LoginCheck(String un,String pw) {
+	System.out.println("Home page")	;
+	
+		if (un =="xyz" && pw=="231dcsd") {
+	}else {
+		System.out.println("Invalid Credentials");
+	}
+	}
+
+}
